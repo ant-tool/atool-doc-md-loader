911be09eb1b8f37d586df26dbe95764015e6d27b
绯一 <jaredlee.ljj@alipay.com>
2016-03-23 17:25:30 +0800

    [feat] add html support

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 const loaderUtils = require('loader-utils');
+// const webpack = require('webpack');
 const MT = require('mark-twain');
 const R = require('ramda');
 const babel = require('babel-core');
 const ejs = require('ejs');
 
-const isCode = R.whereEq({ type: 'code', props: { lang: 'js' } });
+const isCode = R.compose(R.contains(R.__, ['js', 'jsx', 'javascript']), R.path(['props', 'lang']));
 const isStyle = R.whereEq({ type: 'code', props: { lang: 'css' } });
+const isHtml = R.whereEq({ type: 'code', props: { lang: 'html' } });
 const getChildren = R.compose(R.prop('children'), R.defaultTo({}));
 
 const util = require('atool-doc-util');
@@ -33,25 +35,32 @@ module.exports = function(content) {
 
   const code = getChildren(fileContentTree.find(isCode));
   const style = getChildren(fileContentTree.find(isStyle));
+  const html = getChildren(fileContentTree.find(isHtml));
 
   const tpl = query.template;
   this.addDependency(tpl);
+
+  // const common = options.plugins.some(function(i) {
+  //   return i instanceof webpack.optimize.CommonsChunkPlugin;
+  // });
 
   const scripts = [
     path.relative(resourcePath, path.join(options.cwd, options.tplSource, 'common.js')),
     `${path.basename(resourcePath, path.extname(resourcePath))}.js`,
   ];
 
-  const html = ejs.render(fs.readFileSync(tpl, 'utf-8'), {
+  const result = ejs.render(fs.readFileSync(tpl, 'utf-8'), {
     file: {
       title: name,
+      // script: common ? scripts : scripts.slice(1),
       script: scripts,
+      html: html,
       style: style,
       desc: util.marked(fileContentTree),
     },
   });
 
-  this.emitFile(calculateHtmlPath(options.cwd, resourcePath), html);
+  this.emitFile(calculateHtmlPath(options.cwd, resourcePath), result);
 
   return `module.exports = ${babel.transform(code, {
     presets: ['es2015', 'react'],
